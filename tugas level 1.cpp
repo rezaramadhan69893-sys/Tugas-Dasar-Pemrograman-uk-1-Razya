c0c81ce395723f63ab8708a2838c4e2194567a3a
rezaramadhan69893-sys <rezaramadhan69893@gmail.com>
2025-11-05 11:16:58 +0700

    tugas level 1
    Updated variable names for clarity and improved code structure.

--- tugas level 1.cpp
+++ tugas level 1.cpp
@@ -1,20 +1,23 @@
+// NIM : 2500018002
+// NAMA : RAZYA APRIAN
 #include <iostream>
 using namespace std;
 
 int main() {
-    int detik;
+    long long c;
+    // a = jam, b = menit, c = detik, d = sisa_detik, e = detik_akhir
 
     cout<<"Masukkan angka >1 (dalam satuan detik) =";
-    cin>>detik;
-
-    long long jam = detik / 3600;
-    long long sisa_detik = detik % 3600;
-    int menit = sisa_detik / 60;
-    int detik_akhir = sisa_detik % 60;
+    cin>>c;
+    
+    long long a = c / 3600;
+    long long d = c % 3600;
+    int b = d / 60;
+    int e = d % 60;
 
     cout<<endl;
-    cout<<"Hasil konversi adalah :"<<endl;
-    cout<<detik<<" "<<"Detik = "<<" "<<jam<<" jam"<<" "<<menit<<" Menit"<<" "<<detik_akhir<<" detik."<<endl;
+    cout<<"Hasil konversinya adalah :"<<endl;
+    cout<<c<<" "<<"Detik = "<<" "<<a<<" jam,"<<" "<<b<<" Menit,"<<" "<<e<<" detik."<<endl;
     
     return 0;
 }
